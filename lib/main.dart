cfd88b329ca059acb930292182ba7ef4f6515c3d
Shreyashi Das <shreyashidas1styearcse@gmail.com>
2022-12-10 20:08:25 +0530

    Added the add_your_details screen Added the emergency_contacts screen
    Signed-off-by: Shreyashi Das <shreyashidas1styearcse@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:gsos/screens/sign_up.dart';
 // import 'package:gsos/screens/welcome_page.dart';
 import './screens/home.dart';
 import './screens/login.dart';
+import 'screens/add_your_details.dart';
+import 'screens/emergency_contact_list.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -17,6 +19,8 @@ void main() {
     'login': (context) => Mylogin(),
     'signup':(context) => SignUp(),
     'about':(context) => AboutWsos(),
+    'Add_your_details':(context) => AddYourDetails(),
+    'emergency_contacts':(context) => EmergencyContactList(),
     }
   ));
 }
